13b73b92a354e76b8f22248449531ad30757d4f9
Prakash <peeyes@gmail.com>
2026-05-25 21:31:23 +0530

    Allow new frontend origin

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ else:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
